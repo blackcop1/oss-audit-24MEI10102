d60ab2fc47a13c6dbcc3f07982ee8bfda946f3c9
TUSHAR SUDARSHEE <tushar.24mei10102@vitbhopal.ac.in>
2026-03-29 18:27:33 +0530

    Adding README.md and shell scripts to the repository.

---
+++ script1_system_identity_report.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# System Identity Report
+TUSHAR SUDARSHEE, 24MEI10102, VLC, Ubuntu 24.04
+# Additional script content can be added here.
---
+++ script5_manifesto_generator.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Manifesto Generator
+TUSHAR SUDARSHEE, 24MEI10102, VLC, Ubuntu 24.04
+# Additional script content can be added here.
